546fae8b2485fc5aa643a39aa4329f9d00cd4a16
ZumTheZazaKing <zahidi85543@gmail.com>
2021-10-23 23:51:37 +0800

    Styles add ed

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,23 +8,27 @@ export function Main(){
     let { searchQuery, setSearchQuery, results, setResults} = useContext(Context);
 
     useEffect(() => {
-
         if(!searchQuery){
-            fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a')
+            fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=')
             .then(res => res.json()).then(data => {setResults(data.drinks);console.log(data)})
             return;
         }
-
         fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchQuery}`)
         .then(res => res.json())
         .then(data => {setResults(data.drinks);console.log(data)})
     },[searchQuery])
 
     return (<div id="Main">
-        <h1>Drinks & Cocktails</h1>
-        
-        <input type="text" value={searchQuery} onChange={e => setSearchQuery(e.target.value)}/>
-        <button>Search</button>
+        <div id="header">
+            <h1>Drinks & Cocktails</h1>
+            <br/>
+            <input 
+            type="text" 
+            value={searchQuery} 
+            onChange={e => setSearchQuery(e.target.value)}
+            placeholder="Search"
+            />
+        </div>
 
         <div id="results">
             <Suspense fallback={<h1>Loading...</h1>}>
